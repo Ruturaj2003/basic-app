c1415a0dfbfbb1e3c0717c59066d2172f4743d07
Ruturaj2003 <ruturajhb2003@gmail.com>
2023-12-05 21:36:28 +0530

    Create First Component

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { ReactDOM } from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Greeting(params) {
+  return <h2>My First Component</h2>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<Greeting />);
+
+export default Greeting;
