0f5b68bda2ccbf264df55fab8dc2e7d7b57eeb8d
Ruturaj2003 <ruturajhb2003@gmail.com>
2023-12-10 19:34:47 +0530

    Implement List using Map

--- src/index.js
+++ src/index.js
@@ -3,52 +3,38 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const firstBook = {
-  author: ' B. Dylan Hollis',
-  title: 'Baking Yesteryear: The Best Recipes from the 1900s to the 1980s',
-  img: 'https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/81o-PyNHxbL._SY342_.jpg',
-};
-const secondBook = {
-  author: ' Snoop Dogg ',
-  title: "From Crook to Cook: Platinum Recipes from Tha Boss Dogg's Kitchen",
-  img: 'https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/512TiHNyEIL._SX342_SY445_.jpg',
-};
+const books = [
+  {
+    author: ' B. Dylan Hollis',
+    title: 'Baking Yesteryear: The Best Recipes from the 1900s to the 1980s',
+    img: 'https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/81o-PyNHxbL._SY342_.jpg',
+    id: 1,
+  },
+  {
+    author: ' Snoop Dogg ',
+    title: "From Crook to Cook: Platinum Recipes from Tha Boss Dogg's Kitchen",
+    img: 'https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/512TiHNyEIL._SX342_SY445_.jpg',
+    id: 2,
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      >
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
-          repudiandae inventore eos qui animi sed iusto alias eius ea sapiente.
-        </p>
-        <button>click me</button>
-      </Book>
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
     </section>
   );
 }
 
-// const Book = ({ img, title, author, children }) => {
-//   // rest of the logic
-// };
 const Book = (props) => {
-  const { img, title, author, children } = props;
-  console.log(props);
+  const { img, title, author } = props;
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author} </h4>
-      {children}
     </article>
   );
 };
