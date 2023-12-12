c090ad32f8530f0a22a4ff0272bde9936d6c8df2
Ruturaj2003 <ruturajhb2003@gmail.com>
2023-12-12 19:51:51 +0530

    Modularize Components And Add Local Images

---
+++ src/books.js
@@ -0,0 +1,29 @@
+import img3 from './images/img3.jpg';
+import img4 from './images/img4.jpg';
+
+export const books = [
+  {
+    author: 'B. Dylan Hollis',
+    title: 'Baking Yesteryear: The Best Recipes from the 1900s to the 1980s',
+    img: 'https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/81o-PyNHxbL._SY342_.jpg',
+    id: 1,
+  },
+  {
+    author: 'Snoop Dogg ',
+    title: "From Crook to Cook: Platinum Recipes from Tha Boss Dogg's Kitchen",
+    img: 'https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/512TiHNyEIL._SX342_SY445_.jpg',
+    id: 2,
+  },
+  {
+    author: 'David Grann',
+    title: 'The Wager: A Tale of Shipwreck, Mutiny and Murder',
+    img: img3,
+    id: 3,
+  },
+  {
+    author: 'Sarah J. Maas',
+    title: 'A Court of Thorns and Roses ',
+    img: img4,
+    id: 4,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -3,52 +3,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const books = [
-  {
-    author: ' B. Dylan Hollis',
-    title: 'Baking Yesteryear: The Best Recipes from the 1900s to the 1980s',
-    img: 'https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/81o-PyNHxbL._SY342_.jpg',
-    id: 1,
-  },
-  {
-    author: ' Snoop Dogg ',
-    title: "From Crook to Cook: Platinum Recipes from Tha Boss Dogg's Kitchen",
-    img: 'https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/512TiHNyEIL._SX342_SY445_.jpg',
-    id: 2,
-  },
-];
+import { books } from './books';
+import { Book } from './Book';
 
 function BookList() {
-  const getBook = (id) => {
-    const bookie = books.find((book) => {
-      return book.id === id;
-    });
-    console.log(bookie.title);
-  };
-
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book {...book} getBook={getBook} key={book.id} />;
+        return <Book {...book} key={book.id} />;
       })}
     </section>
   );
 }
-
-const Book = (props) => {
-  const { img, title, author, getBook, id } = props;
-  const handleLog = () => {
-    getBook(id);
-  };
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author} </h4>
-      <button onClick={handleLog}>Clicke</button>
-    </article>
-  );
-};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
